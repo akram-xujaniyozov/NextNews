097edceb5d9503ccdecf1fb49305aa945f49e1b4
akram-xujaniyozov <akrom2661@gmail.com>
2025-09-21 13:25:12 +0500

    nested route

--- app/archive/@archive/[[...filter]]/page.jsx
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -1,10 +1,42 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
+import { Fragment } from 'react';
+import Link from 'next/link';
 
-export default function FilteredNewsPage({params}) {
-    const filter = params.filter
-    console.log();
-    
-    const news = getNewsForYear(newYear)
-    return <NewsList news={news} />
+import { getNewsForYear, getAvailableNewsYears } from '@/lib/news';
+import NewsList from '@/components/news-list';
+
+export default function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[0];
+
+  let news;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+  }
+
+  let newsContent = <p>No news found for the selected period</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  const links = getAvailableNewsYears();
+
+  return (
+    <Fragment>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => (
+              <li key={link}>
+                <Link href={`/archive/${link}`}>{link}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </Fragment>
+  );
 }
