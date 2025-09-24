e0f8092fce4cdc4e6c258a5b59ebf9a3b43558fe
akram-xujaniyozov <akrom2661@gmail.com>
2025-09-25 01:02:41 +0500

    adding filter to catch all routes

--- app/archive/@archive/[[...filter]]/page.jsx
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -1,42 +1,58 @@
 import { Fragment } from 'react';
 import Link from 'next/link';
 
-import { getNewsForYear, getAvailableNewsYears } from '@/lib/news';
+import {
+  getNewsForYear,
+  getAvailableNewsYears,
+  getAvailableNewsMonths,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
 import NewsList from '@/components/news-list';
 
 export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
 
   const selectedYear = filter?.[0];
-  const selectedMonth = filter?.[0];
+  const selectedMonth = filter?.[1];
 
   let news;
+  let links = getAvailableNewsYears();
 
   if (selectedYear && !selectedMonth) {
     news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
   }
 
-  // let newsContent = <p>No news found for the selected period</p>;
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period</p>;
 
   if (news && news.length > 0) {
     newsContent = <NewsList news={news} />;
   }
-
-  const links = getAvailableNewsYears();
 
   return (
     <Fragment>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map((link) => (
-              <li key={link}>
-                <Link href={`/archive/${link}`}>{link}</Link>
-              </li>
-            ))}
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
+      {newsContent}
     </Fragment>
   );
 }
