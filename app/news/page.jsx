94a9249854a35d1d778b56654d4423a418d69317
akram-xujaniyozov <akrom2661@gmail.com>
2025-10-26 19:36:25 +0500

    nested routes inside dynamic route

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,7 +1,7 @@
 import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage(params) {
+export default function NewsPage() {
   return (
     <div>
       <h1>News</h1>
